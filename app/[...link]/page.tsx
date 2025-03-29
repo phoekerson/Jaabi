9b07c528ff4fb9dadd854d206160a5f214e3a3cc
Caleb Mintoumba <mintoumbacaleb@gmail.com>
2025-03-29 05:04:28 +0100

    deployment

--- app/[...link]/page.tsx
+++ app/[...link]/page.tsx
@@ -1,53 +1,46 @@
-import React from 'react'
-import Chat from '../components/Chat'
+import React from 'react';
+import Chat from '../components/Chat';
 import { cookies } from 'next/headers';
 import { redis } from '@/lib/redis';
 import { ragChat } from '@/lib/rag-chat';
 
-interface PageProps{
-  params : {
-    link?: string | string[];
-  }
+interface PageProps {
+  params: {
+    link: string[]; // Forcer le type en tableau de chaînes
+  };
 }
-function reconstructUrl({url}: {url: string[]}){
-  const decodedComponents = url.map((component)=>decodeURIComponent(component))
-  return decodedComponents.join('/');
+
+function reconstructUrl(url: string[]) {
+  return url.map(decodeURIComponent).join('/');
 }
-const Page = async({params}: PageProps) => {
 
-  const sessionCookies = (await cookies()).get("sessionId")?.value
-
-
-
-
-  const awaitedParams = params 
-  if(!awaitedParams?.link){
-    return <div> Erreur, veuillez founrir un lien</div>
+const Page = async ({ params }: PageProps) => {
+  if (!params?.link || !Array.isArray(params.link)) {
+    return <div>Erreur, veuillez fournir un lien valide</div>;
   }
-  const linkArray = Array.isArray(awaitedParams.link) ? awaitedParams.link: [awaitedParams.link]
-  const decodedLink = reconstructUrl({url: linkArray})
 
-  const sessionId = (decodedLink + "__" + sessionCookies).replace(/\//g,"")
-  const isAlreadyIndexed = await redis.sismember("indexed-urls", decodedLink)
-  if(!isAlreadyIndexed){
-    console.log("Indexation du lien en cours")
+  const sessionCookies = (await cookies()).get("sessionId")?.value;
+  const decodedLink = reconstructUrl(params.link);
+  const sessionId = (decodedLink + "__" + sessionCookies).replace(/\//g, "");
+
+  const isAlreadyIndexed = await redis.sismember("indexed-urls", decodedLink);
+  if (!isAlreadyIndexed) {
+    console.log("Indexation du lien en cours...");
     await ragChat.context.add({
       type: "html",
       source: decodedLink,
-      config: {chunkOverlap:50, chunkSize: 200}
-    })
-    await redis.sadd("indexed-urls", decodedLink)
+      config: { chunkOverlap: 50, chunkSize: 200 },
+    });
+    await redis.sadd("indexed-urls", decodedLink);
   }
-  const initialMessages = await ragChat.history.getMessages({amount: 10, sessionId})
-  return (
-    <div> 
-      <Chat 
-        decodedLink={decodedLink}
-        sessionId={sessionId}
-        initialMessages={initialMessages}></Chat>
-      
-    </div>
-  )
-}
 
-export default Page
+  const initialMessages = await ragChat.history.getMessages({ amount: 10, sessionId });
+
+  return (
+    <div>
+      <Chat decodedLink={decodedLink} sessionId={sessionId} initialMessages={initialMessages} />
+    </div>
+  );
+};
+
+export default Page;
